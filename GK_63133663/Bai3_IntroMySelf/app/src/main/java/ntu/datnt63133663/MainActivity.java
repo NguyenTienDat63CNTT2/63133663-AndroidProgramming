8f04ecc3fe64b8a7f2c20d23d6fbe141b24a85f0
datnguyen79 <nguyentiendat9913@gmail.com>
2024-03-26 00:45:35 +0700

    Xử lí sự kiện khi người dùng ấn vào icon Facebook
    Thêm mới code

--- GK_63133663/Bai3_IntroMySelf/app/src/main/java/ntu/datnt63133663/MainActivity.java
+++ GK_63133663/Bai3_IntroMySelf/app/src/main/java/ntu/datnt63133663/MainActivity.java
@@ -13,19 +13,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-//    ImageView facebookIcon;
+    ImageView facebookIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        facebookIcon = findViewById(R.id.facebookIcon);
-//        facebookIcon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id=100075085455040"));
-//                startActivity(browserIntent);
-//            }
-//        });
+        facebookIcon = findViewById(R.id.facebookIcon);
+        facebookIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id=100075085455040"));
+                startActivity(browserIntent);
+            }
+        });
     }
 
 }
