59db432a6edd923451e2a1083e7a8d6d6e6d7e2e
datnguyen79 <nguyentiendat9913@gmail.com>
2024-03-26 00:54:10 +0700

    Xử lí sự kiện khi người dùng ấn vào icon Messenger
    thêm mới code

--- GK_63133663/Bai3_IntroMySelf/app/src/main/java/ntu/datnt63133663/MainActivity.java
+++ GK_63133663/Bai3_IntroMySelf/app/src/main/java/ntu/datnt63133663/MainActivity.java
@@ -13,19 +13,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView facebookIcon;
+    ImageView facebook;
+    ImageView mess;
+    ImageView tiktok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        facebookIcon = findViewById(R.id.facebookIcon);
-        facebookIcon.setOnClickListener(new View.OnClickListener() {
+        facebook = findViewById(R.id.facebookIcon);
+        mess = findViewById(R.id.messIcon);
+        tiktok = findViewById(R.id.tiktokIcon);
+
+        facebook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id=100075085455040"));
                 startActivity(browserIntent);
             }
         });
+
+        mess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id=100075085455040"));
+                startActivity(browserIntent);
+            }
+        });
+
     }
 
 }
