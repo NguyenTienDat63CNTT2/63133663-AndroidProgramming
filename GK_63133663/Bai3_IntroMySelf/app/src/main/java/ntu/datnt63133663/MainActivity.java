699e561d873bfd1ae0d67693bfd0c104f592c451
datnguyen79 <nguyentiendat9913@gmail.com>
2024-03-26 00:44:56 +0700

    Giao diện ban đầu
    Thêm mới code

---
+++ GK_63133663/Bai3_IntroMySelf/app/src/main/java/ntu/datnt63133663/MainActivity.java
@@ -0,0 +1,31 @@
+package ntu.datnt63133663;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+//    ImageView facebookIcon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        facebookIcon = findViewById(R.id.facebookIcon);
+//        facebookIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id=100075085455040"));
+//                startActivity(browserIntent);
+//            }
+//        });
+    }
+
+}
