caf9fc7a5b91e0a81f0b517f32e0ffd5f85d7379
datnguyen79 <nguyentiendat9913@gmail.com>
2024-04-16 12:19:45 +0700

    Thực hành RecyclerView
    Thêm mới code

---
+++ RecycleView/app/src/main/java/ntu/datnt/usingrecyclerview/MainActivity.java
@@ -0,0 +1,42 @@
+package ntu.datnt.usingrecyclerview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandScape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //3
+        recyclerViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewLandScape = findViewById(R.id.recyclerLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this,recyclerViewDatas);
+        //7
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<>();
+        dsDuLieu.add(new LandScape("effel","Tháp Eiffel"));
+        dsDuLieu.add(new LandScape("effel","Cung điện Buckingham"));
+        dsDuLieu.add(new LandScape("effel","Cột cờ Hà Nội"));
+        dsDuLieu.add(new LandScape("effel","Tượng nữ thần tự do"));
+        return dsDuLieu;
+    }
+}
