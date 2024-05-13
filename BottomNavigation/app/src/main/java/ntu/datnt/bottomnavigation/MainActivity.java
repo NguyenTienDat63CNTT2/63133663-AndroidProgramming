ca93a75fdb31c5b8c456e6b0d51f1682a8fb0357
datnguyen79 <nguyentiendat9913@gmail.com>
2024-05-13 23:53:29 +0700

    BottomNavigation
    Simble App BottomNavigation

---
+++ BottomNavigation/app/src/main/java/ntu/datnt/bottomnavigation/MainActivity.java
@@ -0,0 +1,48 @@
+package ntu.datnt.bottomnavigation;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
+    }
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        // By using switch we can easily get
+        // the selected fragment
+        // by using there id.
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.algorithm) {
+            selectedFragment = new AlgorithmFragment();
+        } else if (itemId == R.id.course) {
+            selectedFragment = new CourseFragment();
+        } else if (itemId == R.id.profile) {
+            selectedFragment = new ProfileFragment();
+        }
+        // It will help to replace the
+        // one fragment to other.
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    };
+
+}
